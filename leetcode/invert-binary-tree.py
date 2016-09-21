b9a08fce8ae1ab5cc33275fb0ac446f59bdde62b
Rohan Sadale <sadal005@umn.edu>
2016-09-21 00:21:14 -0500

    Trees

---
+++ leetcode/invert-binary-tree.py
@@ -0,0 +1,13 @@
+class Solution(object):
+
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+        root.left, root.right = root.right, root.left
+        return root
